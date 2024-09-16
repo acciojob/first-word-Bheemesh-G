1d6e161c04b01eaa6bc1d581bf87d468c4107d03
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-16 23:31:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	if(s.length===0)
+	{
+		return s;
+		
+	}
+	 const arr = s.trim().split(" ");
+	return arr[0];
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
